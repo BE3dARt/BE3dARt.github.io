d9e05fa58bba7e2ed63590ebf06d14d0b2970fab
BE3dARt <gianluca.imbiscuso@gmail.com>
2020-02-25 10:42:34 +0100

    JavaScript added

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
+var fileInput = document.getElementById('fileInput');
+var fileDisplayArea = document.getElementById('fileDisplayArea');
+var hello = document.getElementById('hello');
+
+hello.addEventListener('click', function () {
+    testus();
+});
+
+function testus() {
+    fileDisplayArea.innerText = 'UFF';
+}
+
+window.onbeforeunload = function () {
+    return "";
+};
+
 window.onload = function() {
-		var fileInput = document.getElementById('fileInput');
-		var fileDisplayArea = document.getElementById('fileDisplayArea');
 
-		fileInput.addEventListener('change', function(e) {
-			var file = fileInput.files[0];
-			var textType = /text.*/;
+    fileInput.addEventListener('change', function(e) {
 
-			if (file.type.match(textType)) {
-				var reader = new FileReader();
+        var file = fileInput.files[0];
+        var reader = new FileReader();
 
-				reader.onload = function(e) {
-					fileDisplayArea.innerText = reader.result;
-				}
-
-				reader.readAsText(file);	
-			} else {
-				fileDisplayArea.innerText = "File not supported!"
-			}
-		});
+        reader.onload = function(e) {
+            fileDisplayArea.innerText = reader.result;
+        }
+        reader.readAsText(file);	
+    });
 }
