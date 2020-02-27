4b35bf5aa20974820f9c984cdde802b5f76755ea
BE3dARt <gianluca.imbiscuso@gmail.com>
2020-02-28 00:47:34 +0100

    Added multiple proxies (with Error Handling) in case one fails[D

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,90 @@
-var fileInput = document.getElementById('fileInput');
-var fileDisplayArea = document.getElementById('fileDisplayArea');
-var hello = document.getElementById('hello');
+    var map = L.map('map').setView([47.486153, 8.206813], 15);
+    var Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+    }).addTo(map);
 
-hello.addEventListener('click', function () {
-    testus();
-});
+    var exportArray = [
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
 
-function testus() {
-    fileDisplayArea.innerText = 'UFF';
-}
+    function addPointToExportArray(inputArray, latitude, longitude) {
+        inputArray[inputArray.length] = [latitude, longitude];
+        return inputArray;
 
-window.onbeforeunload = function () {
-    return "";
-};
+    };
 
-window.onload = function() {
+    var polylineArray = [
+            [0, 0],
+            [0, 0]
+        ];
 
-    fileInput.addEventListener('change', function(e) {
+    var popup = L.popup();
 
-        var file = fileInput.files[0];
-        var reader = new FileReader();
+    function drawPolylineArray(latitude, longitude, altitude) {
 
-        reader.onload = function(e) {
-            fileDisplayArea.innerText = reader.result;
+        var marker = L.marker([latitude, longitude]).addTo(map);
+        marker.bindPopup("<b>Altitude: " + altitude.toFixed(2) + "m</b><br>Latitude: " + latitude.toFixed(2) + " Longitude: " + longitude.toFixed(2)).openPopup();
+
+        if (polylineArray[1][0] != 0) {
+            polylineArray[0] = polylineArray[1];
         }
-        reader.readAsText(file);	
-    });
-}
+
+        polylineArray[1] = [latitude, longitude];
+
+        if (polylineArray[0][0] == 0 || polylineArray[1][0] == 0) {
+
+        } else {
+            var polyline = L.polyline(polylineArray, {
+                color: 'green'
+            }).addTo(map);
+
+        }
+
+    }
+
+    var proxy = ['https://cors-anywhere.herokuapp.com/', 'https://api.allorigins.win/raw?url=', 'https://yacdn.org/proxy/']
+
+    function makeHTTPRequest(latitude, longitude, inputProxy, arrayPos) {
+
+        if (arrayPos <= 2) {
+            var xmlhttp = new XMLHttpRequest();
+
+            xmlhttp.open("GET", inputProxy[arrayPos] + "https://api.opentopodata.org/v1/eudem25m?locations=" + latitude + "," + longitude);
+            xmlhttp.send();
+
+            xmlhttp.onreadystatechange = function () {
+
+                if (this.readyState == 4 && this.status == 200) {
+
+                    var myObj = JSON.parse(this.responseText);
+                    var responseJSON = myObj.results[0].elevation;
+                    drawPolylineArray(latitude, longitude, responseJSON);
+                    return true;
+
+                }
+
+                if (this.readyState == 4 && this.status != 200) {
+                    makeHTTPRequest(latitude, longitude, inputProxy, arrayPos + 1);
+                }
+
+            };
+
+        } else {
+            alert("No proxy worked! Please contact Administartor BE3dARt");
+            return "Error";
+        }
+
+    }
+
+    function onMapClick(e) {
+
+        var latitude = e.latlng.lat;
+        var longitude = e.latlng.lng;
+
+        makeHTTPRequest(latitude, longitude, proxy, 0);
+
+
+    }
+
+    map.on('click', onMapClick);
